47de33b3f2b21f2698c60d1667f4ab410cc35ffc
eyasuyid <eyasuyid@gmail.com>
2022-10-18 16:59:42 +0300

    jason derulo

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''
 This program write user data
-to csv file
+to json file
 '''
 
 import requests
@@ -13,9 +13,14 @@ if __name__ == '__main__':
     emp_data = requests.get(users_url.format(emp_id)).json()
     emp_uname = emp_data.get('username')
     tasks = requests.get("https://jsonplaceholder.typicode.com/todos").json()
-    filename = str(emp_id) + '.csv'
+    filename = str(emp_id) + '.json'
+    tasks_obj = []
     with open(filename, mode='w') as f:
         for task in tasks:
             if task.get('userId') == emp_id:
-                f.write('"{:d}","{:s}","{}","{:s}"\n'.format(emp_id,
-                        emp_uname, task.get('completed'), task.get('title')))
+                task_obj = {"task": task.get('title'),
+                            "completed": task.get('completed'),
+                            "username": emp_uname}
+                tasks_obj.append(task_obj)
+        json_obj = {str(emp_id): tasks_obj}
+        f.write("{}".format(json_obj))
