4aa991e0bb1c0b37c38348af78426f2b24c8d531
eyasuyid <eyasuyid@gmail.com>
2022-11-02 12:10:45 +0300

    prepare for revert

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+<<<<<<< HEAD
 """Module for task 0"""
 
 
@@ -15,3 +16,22 @@ def number_of_subscribers(subreddit):
         return 0
 
     return sub_info.json().get("data").get("subscribers")
+=======
+"""Module for reddit subs"""
+
+
+def number_of_subscribers(subreddit):
+    """"
+        A method that return the number of
+        reddit subscribers
+    """
+    import requests
+
+    result = requests.get("https://www.reddit.com/r/{}/about.json"
+                          .format(subreddit),
+                          headers={"User-Agent": "My-User-Agent"},
+                          allow_redirects=False)
+    if result.status_code >= 300:
+        return 0
+    return result.json().get("date").get("subscribers")
+>>>>>>> parent of c55dbed... reddit subs
