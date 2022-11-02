41059c208d1ebd2b3a6ee7eb7e9fd9b8989beb6b
eyasuyid <eyasuyid@gmail.com>
2022-11-02 12:21:53 +0300

    less edit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+'''Module for task 0'''
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """"A method that return the number of
-    reddit subscribers"""
+    '''
+    Queries the Reddit API and returns the number of subscribers
+    to the subreddit
+    '''
 
-    result = requests.get("https://www.reddit.com/r/{}/about.json"
+    sub_info = requests.get('https://www.reddit.com/r/{}/about.json'
                             .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
+                            headers={'User-Agent': 'My-User-Agent'},
                             allow_redirects=False)
-    if result.status_code >= 300:
+    if sub_info.status_code >= 300:
         return 0
-    return result.json().get("date").get("subscribers")
+
+    return sub_info.json().get('data').get('subscribers')
