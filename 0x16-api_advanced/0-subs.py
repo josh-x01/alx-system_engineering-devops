742194654e9ebb8a6fc7a09ea94cbc1d023bf164
eyasuyid <eyasuyid@gmail.com>
2022-11-02 12:17:31 +0300

    reverting 0-c

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,9 +8,9 @@ def number_of_subscribers(subreddit):
     reddit subscribers"""
 
     result = requests.get("https://www.reddit.com/r/{}/about.json"
-                          .format(subreddit),
-                          headers={"User-Agent": "My-User-Agent"},
-                          allow_redirects=False)
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
     if result.status_code >= 300:
         return 0
     return result.json().get("date").get("subscribers")
