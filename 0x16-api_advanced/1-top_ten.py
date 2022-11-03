db24591d99ed5c471e199f7e5b0943e365d4265c
eyasuyid <eyasuyid@gmail.com>
2022-11-03 20:50:49 +0300

    Revert "1-top"
    This reverts commit 450d1da7a290c87f4ad4dc4336027a74759c02a6.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-'''Module for task 1'''
+"""Module for task 1"""
 
 
 def top_ten(subreddit):
-    '''method that print the top
-    hot ranks'''
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
     import requests
 
-    results = requests.get('https://www.reddit.com/r/{}/hot.json?limit=10'
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
                             .format(subreddit),
-                            headers={'User-Agent': 'My-User-Agent'},
+                            headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
-    if results.status_code >= 300:
+    if sub_info.status_code >= 300:
         print('None')
     else:
-        [print(result.get('data').get('title'))
-         for result in results.json().get('data').get('children')]
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
